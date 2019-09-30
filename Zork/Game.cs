f7a8914ad61bc8109f40f8b196cd8fda1d1a4652
Ironman407 <42649627+Ironman407@users.noreply.github.com>
2019-09-30 15:41:30 -0400

    Page 14 WIP not 12

--- Zork/Game.cs
+++ Zork/Game.cs
@@ -34,47 +34,13 @@ namespace Zork
 
         public Game() => CommandManager = new CommandManager();
 
-        private void LoadCommands()
-        {
-            var commandMethods = (from type in Assembly.GetExecutingAssembly().GetTypes()
-                                  from method in type.GetMethods()
-                                  let attribute = method.GetCustomAttribute<CommandAttribute>()
-                                  where type.IsClass && type.GetCustomAttribute<CommandClassAttribute>() != null
-                                  where attribute != null
-                                  select new Command(attribute.CommandName, attribute.Verbs,
-                                  (Action<Game, CommandContext>)Delegate.CreateDelegate(typeof(Action<Game, CommandContext>), method)));
-
-            CommandManager.AddCommands(commandMethods);
-        }
-
-        private void LoadScripts()
-        {
-            foreach(string file in Directory.EnumerateFiles(ScriptDirectory, ScriptFileExtension))
-            {
-                try
-                {
-                    var scriptOptions = ScriptOptions.Default.AddReferences(Assembly.GetExecutingAssembly());
-#if (DEBUG)
-                    scriptOptions = scriptOptions.WithEmitDebugInformation(true)
-                        .WithFilePath(new FileInfo(file).FullName)
-                        .WithFileEncoding(Encoding.UTF8);
-#endif
-                    string script = File.ReadAllText(file);
-                    CSharpScript.RunAsync(script, scriptOptions).Wait();
-                }
-                catch(Exception ex)
-                {
-                    Console.WriteLine($"Error compiling script: {file} Error: {ex.Message}");
-                }
-            }
-        }
 
         private static readonly string ScriptDirectory = "Scripts";
         private static readonly string ScriptFileExtension = "*.csx";
 
         public void Run()
         {
-            IsRunning = true;
+            mIsRunning = true;
             Room previousRoom = null;
             while (IsRunning)
             {
@@ -97,7 +63,14 @@ namespace Zork
             }
         }
 
+        public void restart()
+        {
+            mIsRunning = false;
+            mIsRestarting = true;
+            Console.Clear;
+        }
 
+        public void Quit() mIsrunning = false;
 
 
         public static Game Load(string filename)
@@ -106,6 +79,41 @@ namespace Zork
             game.Player = game.World.SpawnPlayer();
 
             return game;
+        }
+
+        private void LoadCommands()
+        {
+            var commandMethods = (from type in Assembly.GetExecutingAssembly().GetTypes()
+                                  from method in type.GetMethods()
+                                  let attribute = method.GetCustomAttribute<CommandAttribute>()
+                                  where type.IsClass && type.GetCustomAttribute<CommandClassAttribute>() != null
+                                  where attribute != null
+                                  select new Command(attribute.CommandName, attribute.Verbs,
+                                  (Action<Game, CommandContext>)Delegate.CreateDelegate(typeof(Action<Game, CommandContext>), method)));
+
+            CommandManager.AddCommands(commandMethods);
+        }
+
+        private void LoadScripts()
+        {
+            foreach (string file in Directory.EnumerateFiles(ScriptDirectory, ScriptFileExtension))
+            {
+                try
+                {
+                    var scriptOptions = ScriptOptions.Default.AddReferences(Assembly.GetExecutingAssembly());
+#if (DEBUG)
+                    scriptOptions = scriptOptions.WithEmitDebugInformation(true)
+                        .WithFilePath(new FileInfo(file).FullName)
+                        .WithFileEncoding(Encoding.UTF8);
+#endif
+                    string script = File.ReadAllText(file);
+                    CSharpScript.RunAsync(script, scriptOptions).Wait();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error compiling script: {file} Error: {ex.Message}");
+                }
+            }
         }
     }
 }
